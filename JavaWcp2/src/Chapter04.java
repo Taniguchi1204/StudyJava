2d03db93db02174fb81793937cfcbe222de14e82
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-10-26 16:01:42 +0900

    Chapter04

---
+++ JavaWcp2/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "谷口";
+		System.out.println(name);
+		
+		name = "aaa";
+		System.out.println(name);
+		
+		final int number = 7;
+		System.out.println(number);
+	}
+}
