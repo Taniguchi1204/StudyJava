f718a328099ed3b428e38c0805b7f89ca33dd35c
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-10-27 22:28:42 +0900

    配列

---
+++ JavaWcp2/src/array.java
@@ -0,0 +1,22 @@
+
+public class array {
+	public static void main(String[] args) {
+		int[] array1 = new int[3];
+		array1[0] = 60;
+		array1[2] = 20;
+		array1[1] = 20;
+		int int2 = array1[2];
+		
+		int array2[] = null;
+		
+		int[] array3 = new int[] { 20, 30,50 };
+		int[] array4 = {10,20,30};
+		
+		System.out.println(int2);
+		
+		System.out.println(array1);
+		System.out.println(array2);
+		System.out.println(array3);
+		System.out.println(array4);
+	}
+}
