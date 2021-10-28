2da9cf5925e31ce526767c1663f124f29871a09e
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-10-28 22:21:16 +0900

    chapter5

---
+++ JavaWcp2/src/Chapter5.java
@@ -0,0 +1,22 @@
+
+public class Chapter5 {
+	public static void main(String[] args) {
+		long l = 1000000000000l;
+		double d = 3.14;
+		boolean b = true;
+		Integer i = 100;
+		Float f = 14142f;
+		Character c = 'う';
+		int[] array = {0,10,20,30,40,50};
+		
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(array[2]);
+
+	}
+
+}
